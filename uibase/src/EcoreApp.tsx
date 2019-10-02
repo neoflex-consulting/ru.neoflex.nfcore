1475689ac14980de331b8326e6405f195e237bfe
michael-whi <michael_tugushev@mail.ru>
2019-10-02 15:11:16 +0300

    Autohiding marks.

--- uibase/src/EcoreApp.tsx
+++ uibase/src/EcoreApp.tsx
@@ -113,8 +113,10 @@ class EcoreApp extends React.Component<any, State> {
                                 }
                             </Menu.SubMenu>
                             <Menu.SubMenu  title={<span><Icon type="notification" style={{fontSize: '17px'}}/>Notification</span>}>
-                                <Menu.Item key={'showNotifications'}><Icon type="eye" style={{fontSize: '17px'}} />Show constantly</Menu.Item>
-                                <Menu.Item key={'autoHideNotifications'}><Icon type="clock-circle" style={{fontSize: '17px'}} />Autohide</Menu.Item>
+                                {localStorage.getItem('notifierDuration') === '3' ?
+                                    <Menu.Item key={'showNotifications'}><Icon type="eye" style={{fontSize: '17px'}} />Disable autohiding</Menu.Item>
+                                :
+                                    <Menu.Item key={'autoHideNotifications'}><Icon type="clock-circle" style={{fontSize: '17px'}} />Autohide</Menu.Item>}
                             </Menu.SubMenu>
                         </Menu.SubMenu>
                     </Menu>
