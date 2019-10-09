5a96153a3c90a2f3076ace31f5c2ed30c02adf91
arch7tect <arch7tect@gmail.com>
2019-10-10 01:02:15 +0300

    git db. first try

---
+++ gitdb/src/main/java/ru/neoflex/meta/GitdbApplication.java
@@ -0,0 +1,13 @@
+package ru.neoflex.meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitdbApplication.class, args);
+    }
+
+}
