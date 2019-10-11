16505c18135e98b9be11c35fdff38108b56a9b71
hedaira <mezzo666@mail.ru>
2019-10-12 00:33:15 +0300

    /

--- uibase/src/components/app/Calendar.tsx
+++ uibase/src/components/app/Calendar.tsx
@@ -149,51 +149,6 @@ class Calendar extends React.Component<Props & WithTranslation, State> {
         return temp;
     }
 
-    renderLegend() {
-        const stat: { push(div: any): void } = [];
-            statues.filter( status =>
-                stat.push(
-                    <div>
-                        <Tag style={{
-                            display: "table-caption",
-                            width: "300px",
-                            textAlign: "left",
-                            backgroundColor: `${this.selectStatusColor(status)}`,
-                        }}
-                        >
-                            {status}
-                        </Tag>
-                    </div>
-                )
-            );
-        return (
-            <div>
-                <Button
-                onClick = { () => {
-                    let btn = (<Button type="link" size="small" onClick={() => notification.destroy()}>
-                        Close All
-                    </Button>);
-                    notification.open({
-                        message: "Легенда",
-                        description: stat,
-                        duration: 0,
-                        key: "single",
-                        btn,
-                        style: {
-                            width: 400,
-                            marginLeft: -10,
-                            marginTop: 16,
-                            wordWrap: "break-word"
-                        },
-                    })}}
-                style={{width: "300px"}}
-                >
-                    Легенда
-                </Button>
-            </div>
-        )
-    }
-
     selectStatusColor = (status: string): any => {
         let colorButton: any;
         colorList
@@ -228,15 +183,10 @@ class Calendar extends React.Component<Props & WithTranslation, State> {
 
     render() {
         return (
-            <div>
-                <div className="calendar">
-                    {this.renderHeader()}
-                    {this.renderDays()}
-                    {this.renderCells()}
-                </div>
-                <div style={{display: "table-caption"}}>
-                    {this.renderLegend()}
-                </div>
+            <div className="calendar">
+                {this.renderHeader()}
+                {this.renderDays()}
+                {this.renderCells()}
             </div>
         );
     }
