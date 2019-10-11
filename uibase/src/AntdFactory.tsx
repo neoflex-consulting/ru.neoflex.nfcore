7077c5a2f778ae2c90d5e36e0da065b4976a8de3
hedaira <mezzo666@mail.ru>
2019-10-11 17:07:39 +0300

    added ComponentElement in AntdFactory

--- uibase/src/AntdFactory.tsx
+++ uibase/src/AntdFactory.tsx
@@ -1,14 +1,15 @@
-
 import {ViewFactory, View} from './View'
 import Ecore from "ecore";
 import * as React from "react";
 import {Col, Row} from "antd";
+import UserComponent from "./components/app/UserComponent";
 
 abstract class ViewContainer extends View {
     renderChildren = () => {
-        const children = this.viewObject.get("children") as Ecore.EObject[]
+        const children = this.viewObject.get("children") as Ecore.EObject[];
         return children.map(c=>this.viewFactory.createView(c, this.props))
-    }
+    };
+
     render = () => {
         return <div>{this.renderChildren()}</div>
     }
@@ -38,14 +39,21 @@ class Col_ extends ViewContainer {
     }
 }
 
+class ComponentElement_ extends ViewContainer {
+    render = () => {
+        return <UserComponent componentClassName="MandatoryReportingTrans"/>
+    }
+}
+
 class AntdFactory implements ViewFactory {
-    name = 'antd'
-    components = new Map<string, typeof View>()
+    name = 'antd';
+    components = new Map<string, typeof View>();
     constructor() {
-        this.components.set('ru.neoflex.nfcore.application#//Div', Div_)
-        this.components.set('ru.neoflex.nfcore.application#//Span', Span_)
-        this.components.set('ru.neoflex.nfcore.application#//Row', Row_)
-        this.components.set('ru.neoflex.nfcore.application#//Col', Col_)
+        this.components.set('ru.neoflex.nfcore.application#//Div', Div_);
+        this.components.set('ru.neoflex.nfcore.application#//Span', Span_);
+        this.components.set('ru.neoflex.nfcore.application#//Row', Row_);
+        this.components.set('ru.neoflex.nfcore.application#//Col', Col_);
+        this.components.set('ru.neoflex.nfcore.application#//ComponentElement', ComponentElement_)
     }
     createView = (viewObject: Ecore.EObject, props: any) => {
         let Component = this.components.get(viewObject.eClass.eURI());
