dea886e92a7e1247d6af71211a8ec69d73a2c15e
hedaira <mezzo666@mail.ru>
2019-10-23 18:35:12 +0300

    fix AntdFactory

--- uibase/src/AntdFactory.tsx
+++ uibase/src/AntdFactory.tsx
@@ -1,13 +1,15 @@
 import {ViewFactory, View} from './View'
 import Ecore from "ecore";
 import * as React from "react";
-import {Col, Row} from "antd";
+import {Col, Form, Row, Tabs} from "antd";
 import UserComponent from "./components/app/UserComponent";
 
 abstract class ViewContainer extends View {
     renderChildren = () => {
-        const children = this.viewObject.get("children") as Ecore.EObject[];
-        return children.map(c=>this.viewFactory.createView(c, this.props))
+        let children = this.viewObject.get("children") as Ecore.EObject[];
+        let childrenView = children.map(
+            (c: Ecore.EObject) => this.viewFactory.createView(c, this.props));
+        return <div>{childrenView}</div>
     };
 
     render = () => {
@@ -16,26 +18,66 @@ abstract class ViewContainer extends View {
 }
 
 class Div_ extends ViewContainer {
+
     render = () => {
-        return <div>{this.renderChildren()}</div>
+        return (
+            <div>
+                {this.renderChildren()}
+            </div>
+        )
     }
 }
 
 class Span_ extends ViewContainer {
     render = () => {
-        return <span>{this.renderChildren()}</span>
+        return (
+            <span>
+                {this.renderChildren()}
+            </span>
+        )
     }
 }
 
 class Row_ extends ViewContainer {
     render = () => {
-        return <Row>{this.renderChildren()}</Row>
+        return (
+            <Row>
+                {this.renderChildren()}
+            </Row>
+        )
     }
 }
 
 class Col_ extends ViewContainer {
     render = () => {
-        return <Col>{this.renderChildren()}</Col>
+        return (
+            <Col>
+                {this.renderChildren()}
+            </Col>
+            )
+    }
+}
+
+class Form_ extends ViewContainer {
+    render = () => {
+        return (
+            <Form>
+                {this.renderChildren()}
+            </Form>
+            )
+    }
+}
+
+class Tabs_ extends ViewContainer {
+    render = () => {
+        const { TabPane } = Tabs;
+        return (
+            <Tabs>
+                <TabPane tab={`${this.viewObject.get('code')}`}>
+                {this.renderChildren()}
+                </TabPane>
+            </Tabs>
+        )
     }
 }
 
@@ -57,15 +99,20 @@ class AntdFactory implements ViewFactory {
         this.components.set('ru.neoflex.nfcore.application#//Div', Div_);
         this.components.set('ru.neoflex.nfcore.application#//Span', Span_);
         this.components.set('ru.neoflex.nfcore.application#//Row', Row_);
-        this.components.set('ru.neoflex.nfcore.application#//Col', Col_);
-        this.components.set('ru.neoflex.nfcore.application#//ComponentElement', ComponentElement_)
+        this.components.set('ru.neoflex.nfcore.application#//Column', Col_);
+        this.components.set('ru.neoflex.nfcore.application#//ComponentElement', ComponentElement_);
+        this.components.set('ru.neoflex.nfcore.application#//Form', Form_);
+        this.components.set('ru.neoflex.nfcore.application#//Tabs', Tabs_);
     }
-    createView = (viewObject: Ecore.EObject, props: any) => {
+
+    createView(viewObject: Ecore.EObject, props: any): JSX.Element {
         let Component = this.components.get(viewObject.eClass.eURI());
         if (!Component) {
             Component = View
         }
-        return <Component viewObject={viewObject} vievFactory={this} {...props}/>
+        return (
+            <Component {...props} key={viewObject.get('uri')} viewObject={viewObject} viewFactory={this}/>
+        )
     }
 }
 
