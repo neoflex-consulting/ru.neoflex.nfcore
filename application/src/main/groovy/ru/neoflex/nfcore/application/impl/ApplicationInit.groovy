08c4eae901d74c7237bb92800c3f9053b8cb185a
hedaira <mezzo666@mail.ru>
2019-10-24 11:37:02 +0300

    autoAdd Calendar in ReportsApp on build Application

--- application/src/main/groovy/ru/neoflex/nfcore/application/impl/ApplicationInit.groovy
+++ application/src/main/groovy/ru/neoflex/nfcore/application/impl/ApplicationInit.groovy
@@ -10,17 +10,19 @@ import ru.neoflex.nfcore.base.util.DocFinder
 import ru.neoflex.nfcore.reports.ReportsPackage
 
 class ApplicationInit {
-    static def findOrCreateEObject(EClass eClass, String name, boolean replace = false) {
+    static def findOrCreateEObject(EClass eClass, String name, String componentClassName, boolean replace = false) {
         def rs = DocFinder.create(Context.current.store, eClass, [name: name])
-                .execute().resourceSet
-        while (replace && !rs.resources.empty) {
-            Context.current.store.deleteResource(rs.resources.remove(0).getURI())
-        }
-        if (rs.resources.empty) {
-            def eObject = EcoreUtil.create(eClass)
-            eObject.eSet(eClass.getEStructuralFeature("name"), name)
-            rs.resources.add(Context.current.store.createEObject(eObject))
-        }
+                    .execute().resourceSet
+
+            while (replace && !rs.resources.empty) {
+                Context.current.store.deleteResource(rs.resources.remove(0).getURI())
+            }
+            if (rs.resources.empty) {
+                def eObject = EcoreUtil.create(eClass)
+                eObject.eSet(eClass.getEStructuralFeature("name"), name)
+                if (componentClassName != "undefined") {eObject.eSet(eClass.getEStructuralFeature("componentClassName"), componentClassName)}
+                rs.resources.add(Context.current.store.createEObject(eObject))
+            }
         return rs.resources.get(0).contents.get(0)
     }
 
@@ -41,14 +43,16 @@ class ApplicationInit {
             }
             def reportNode = ApplicationFactory.eINSTANCE.createEObjectNode()
             reportNode.code = 'report1'
-            def report1 = findOrCreateEObject(ReportsPackage.Literals.REPORT, "report1", true)
+            def report1 = findOrCreateEObject(ReportsPackage.Literals.REPORT, "report1", "undefined",true)
             reportNode.setEObject(report1)
             (referenceTree.children[0] as CatalogNode).children.add(reportNode)
             application.setReferenceTree(referenceTree)
-            def userComponent1 = findOrCreateEObject(ApplicationPackage.Literals.USER_COMPONENT, "Mandatory Reporting", true)
             def componentElement = ApplicationFactory.eINSTANCE.createComponentElement()
-            application.view = componentElement
+            componentElement.code = 'Mandatory Reporting'
+            def userComponent1 = findOrCreateEObject(ApplicationPackage.Literals.USER_COMPONENT, "Mandatory Reporting", "MandatoryReportingTrans", true)
             componentElement.setComponent(userComponent1)
+            application.view = componentElement
+
             rs.resources.add(Context.current.store.createEObject(application))
         }
         return rs.resources.get(0).contents.get(0)
