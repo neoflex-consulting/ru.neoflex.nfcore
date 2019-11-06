c761be8e238723b67c3989c6fe35bb6978f120b4
Oleg Orlov <orlov@neoflex.ru>
2019-11-06 19:01:35 +0300

    GitDB UI

--- uibase/src/components/GitDB.tsx
+++ uibase/src/components/GitDB.tsx
@@ -1,18 +1,24 @@
 import * as React from "react";
-// import {Form, Button, Tooltip} from 'antd';
+import {Row, Col, Table, Checkbox, Button, Tooltip} from 'antd';
 // import { Ecore } from "ecore";
-// import { API } from "../modules/api";
+import { API } from "../modules/api";
 // import {Icon as IconFA} from 'react-fa';
 // import AceEditor from "react-ace";
 import 'brace/mode/json';
 import 'brace/theme/tomorrow';
 // import Splitter from './CustomSplitter'
-import {withTranslation} from "react-i18next";
+import {WithTranslation, withTranslation} from "react-i18next";
+const {Column} = Table;
+const ButtonGroup = Button.Group
 
-export interface Props {
-}
+interface Props {}
 
 interface State {
+    branchInfo: {
+        current: string,
+        default: string,
+        branches: string[]
+    }
 }
 
 class GitDB extends React.Component<any, State> {
@@ -21,12 +27,69 @@ class GitDB extends React.Component<any, State> {
     //     super(props);
     // }
 
-    state = {};
+    state = {
+        branchInfo: {
+            current: "master",
+            default: 'master',
+            branches: ["master"]
+        }
+    };
+
+    componentDidMount(): void {
+        this.fetchBranchInfo()
+    }
+
+    componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<State>, snapshot?: any): void {
+        this.fetchBranchInfo()
+    }
+
+    fetchBranchInfo = () => {
+        API.instance().fetchJson("/system/branch").then(branchInfo => {
+            this.setState({branchInfo})
+        })
+    }
+
+    putCurrentBranch = (branch: string) => {
+        API.instance().fetchJson("/system/branch/" + branch).then(branchInfo => {
+            this.setState({branchInfo})
+        })
+    }
 
     render() {
-        // const {t} = this.props as Props & WithTranslation;
+        const {t} = this.props as Props & WithTranslation;
+        const branches = this.state.branchInfo.branches.map(branch => ({branch, isCurrent: branch === this.state.branchInfo.current, isDefault: this.state.branchInfo.default}))
         return (
-            <div>GitDB!</div>
+            <Row>
+                <Col span={3}/>
+                <Col span={18}>
+                    <Table pagination={false} size="small" dataSource={branches}>
+                        <Column title={"Branch"}
+                                dataIndex={"branch"} key={"branch"}
+                        />
+                        <Column
+                            title={"Is Current"}
+                            dataIndex={"isCurrent"} key={"isCurrent"}
+                            render={(text) => <Checkbox disabled={true} checked={text === true}/>}
+                        />
+                        <Column title={"Is Default"}
+                                dataIndex={"isDefault"} key={"isDefault"}
+                                render={(text) => <Checkbox disabled={true} checked={text === true}/>}
+                        />
+                        <Column key={"command"}
+                                render={branch=>(
+                                    <ButtonGroup className="pull-right">
+                                        <Tooltip title={"Set Current"}>
+                                            <Button type="dashed" size="small" >
+
+                                            </Button>
+                                        </Tooltip>
+                                    </ButtonGroup>
+                                )}
+                        />
+                    </Table>
+                </Col>
+                <Col span={3}/>
+            </Row>
         );
     }
 }
