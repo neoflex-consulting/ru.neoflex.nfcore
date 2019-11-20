2eb44e205d344e0f09729a1b00b43fccd0b0b659
Oleg Orlov <orlov@neoflex.ru>
2019-11-20 13:43:54 +0300

    Validation framework in progress

--- base/src/main/groovy/ru/neoflex/nfcore/base/auth/impl/AuthValidatorExt.groovy
+++ base/src/main/groovy/ru/neoflex/nfcore/base/auth/impl/AuthValidatorExt.groovy
@@ -5,20 +5,20 @@ import org.eclipse.emf.common.util.DiagnosticChain
 import ru.neoflex.nfcore.base.auth.Authorization
 import ru.neoflex.nfcore.base.auth.util.AuthValidator
 
-class AuthValidatorExt extends AuthValidator  {
+class AuthValidatorExt extends AuthValidator {
     @Override
     boolean validateAuthorization_IsValid(Authorization authorization, DiagnosticChain diagnostics, Map<Object, Object> context) {
         if (authorization.name == null || authorization.name.length() < 3) {
             if (diagnostics != null) {
-                diagnostics.add
-                (createDiagnostic
-                        (Diagnostic.ERROR,
-                                DIAGNOSTIC_SOURCE,
-                                0,
-                                "_UI_GenericConstraint_diagnostic",
-                                ["IsValid", getObjectLabel(authorization, context)] as Object[],
-                                [authorization] as Object[],
-                                context));
+                diagnostics.add(createDiagnostic(
+                        Diagnostic.ERROR,
+                        DIAGNOSTIC_SOURCE,
+                        0,
+                        "_UI_GenericConstraint_diagnostic",
+                        ["IsValid", getObjectLabel(authorization, context)] as Object[],
+                        [authorization] as Object[],
+                        context
+                ));
             }
             return false;
         }
