abb47d2dfe9f25218bfaffaed43db3ad3f1d8d0c
hedaira <mezzo666@mail.ru>
2019-11-26 13:36:39 +0300

    rename datasource to dataset

--- application/src/main/java/ru/neoflex/nfcore/application/components/ApplicationModuleRegistry.java
+++ application/src/main/java/ru/neoflex/nfcore/application/components/ApplicationModuleRegistry.java
@@ -1,18 +1,13 @@
 package ru.neoflex.nfcore.application.components;
 
-import org.eclipse.emf.ecore.EPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import ru.neoflex.nfcore.application.ApplicationPackage;
-import ru.neoflex.nfcore.base.components.IModuleRegistry;
 import ru.neoflex.nfcore.base.components.ModuleRegistryImpl;
-import ru.neoflex.nfcore.datasource.DatasourcePackage;
+import ru.neoflex.nfcore.dataset.DatasetPackage;
 import ru.neoflex.nfcore.locales.LocalesPackage;
 import ru.neoflex.nfcore.reports.ReportsPackage;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @ComponentScan("ru.neoflex.nfcore")
@@ -20,7 +15,7 @@ import java.util.List;
 public class ApplicationModuleRegistry extends ModuleRegistryImpl {
     ApplicationModuleRegistry() {
         registerEPackage(ApplicationPackage.eINSTANCE);
-        registerEPackage(DatasourcePackage.eINSTANCE);
+        registerEPackage(DatasetPackage.eINSTANCE);
         registerEPackage(LocalesPackage.eINSTANCE);
         registerEPackage(ReportsPackage.eINSTANCE);
     }
