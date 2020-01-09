8b81ee9a984d94a83fba9699fd00bac2f756d466
arch7tect <arch7tect@gmail.com>
2020-01-09 16:11:18 +0300

    orientdb - minor refactoring, change version to 3.0.27

--- emf-orientdb/src/test/java/ru/neoflex/meta/emforientdb/LuceneTests.java
+++ emf-orientdb/src/test/java/ru/neoflex/meta/emforientdb/LuceneTests.java
@@ -6,9 +6,13 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import ru.neoflex.meta.test.Placemark;
-import ru.neoflex.meta.test.OPoint;
-import ru.neoflex.meta.test.TestFactory;
+import org.noggit.JSONParser;
+import org.noggit.ObjectBuilder;
+import ru.neoflex.meta.test.*;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class LuceneTests extends TestBase {
 
@@ -27,11 +31,11 @@ public class LuceneTests extends TestBase {
         Resource resource = server.inTransaction(session -> {
             ResourceSet rs = session.createResourceSet();
             Placemark placemark = TestFactory.eINSTANCE.createPlacemark();
-            placemark.setName("Test Place");
-            placemark.setDescription("My Test Place");
+            placemark.setName("Trivento (Cathedral)");
+            placemark.setDescription("[IT-86029] Trivento (Cathedral) [I-Trivento]");
             OPoint point = TestFactory.eINSTANCE.createOPoint();
-            point.getCoordinates().add(1.0);
-            point.getCoordinates().add(2.0);
+            point.getCoordinates().add(14.55126);
+            point.getCoordinates().add(41.7834853);
             placemark.setPoint(point);
             Resource placeRes = rs.createResource(server.createURI());
             placeRes.getContents().add(placemark);
@@ -39,9 +43,44 @@ public class LuceneTests extends TestBase {
             return placeRes;
         });
         Placemark placemark = server.inTransaction(session -> {
-            Resource r = session.query("select from test_Placemark where name=?", "Test Place").get(0);
+            Resource r = session.query("select from test_Placemark where name=?", "Trivento (Cathedral)").get(0);
             return (Placemark) r.getContents().get(0);
         });
-        Assert.assertEquals("My Test Place", placemark.getDescription());
+        Assert.assertEquals("[IT-86029] Trivento (Cathedral) [I-Trivento]", placemark.getDescription());
+    }
+
+//    @Test
+    public void testLoadCountries() throws Exception {
+        InputStream is;
+        try {
+            is = new FileInputStream("C:/Users/arch7/Downloads/gadm36_levels_shp/gadm36_0.json");
+        }
+        catch (Throwable e) {
+            return;
+        }
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(is, "utf-8"))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                Map map = (Map) ObjectBuilder.getVal(new JSONParser(line));
+                Map properties = (Map) map.get("properties");
+                String gid = (String) properties.get("GID_0");
+                String name = (String) properties.get("NAME_0");
+                Map geometry = (Map) map.get("geometry");
+                String type = (String) geometry.get("type");
+                ArrayList coordinates = (ArrayList) geometry.get("coordinates");
+                Country country = TestFactory.eINSTANCE.createCountry();
+                country.setGid(gid);
+                country.setName(name);
+                System.out.println(country.getName());
+                OMultiPolygon g = TestFactory.eINSTANCE.createOMultiPolygon();
+                g.getCoordinates().addAll(coordinates);
+                country.setGeometry(g);
+                server.inTransaction(session -> {
+                    ResourceSet rs = session.createResourceSet();
+                    Resource resource = rs.createResource(server.createURI());
+                    resource.getContents().add(country);
+                    resource.save(null);
+                });
+            }
+        }
     }
 }
