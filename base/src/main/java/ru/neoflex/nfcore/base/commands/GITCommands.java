0b9e6daed4126fcfcc0059cb49c099935e45e748
arch7tect <arch7tect@gmail.com>
2020-01-20 01:57:01 +0300

    git-cd, git-pwd, git-ls

--- base/src/main/java/ru/neoflex/nfcore/base/commands/GITCommands.java
+++ base/src/main/java/ru/neoflex/nfcore/base/commands/GITCommands.java
@@ -4,17 +4,24 @@ import com.github.fonimus.ssh.shell.SshShellHelper;
 import com.github.fonimus.ssh.shell.commands.SshShellComponent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+import ru.neoflex.meta.emfgit.Transaction;
 import ru.neoflex.nfcore.base.services.Workspace;
 
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SshShellComponent
 public class GITCommands {
     @Autowired
     Workspace workspace;
     private final SshShellHelper helper;
+    private String wd = "/";
 
     public GITCommands(SshShellHelper helper) {
         this.helper = helper;
@@ -36,6 +43,37 @@ public class GITCommands {
     public List<String> gitSetBranch(String name) throws IOException {
         workspace.setCurrentBranch(name);
         return gitBranchInfo();
+    }
+
+    @ShellMethod("List (working) directory")
+    public String gitLs(@ShellOption(defaultValue=ShellOption.NULL) String dir) throws Exception {
+        return workspace.getDatabase().inTransaction(workspace.getCurrentBranch(), Transaction.LockType.READ, tx -> {
+            FileSystem fs = tx.getFileSystem();
+            Path workingDir = fs.getPath(dir == null ? wd : dir);
+            return Files.list(workingDir).map(path ->
+                    Files.isDirectory(path) ? path.getFileName().toString() + "/" : path.getFileName().toString())
+                    .collect(Collectors.joining(" "));
+        });
+    }
+
+    @ShellMethod("Print working directory")
+    public String gitPwd(@ShellOption(defaultValue=ShellOption.NULL) String dir) throws Exception {
+        return wd;
+    }
+
+    @ShellMethod("Change working directory")
+    public String gitCd(String dir) throws Exception {
+        Path wdPath = workspace.getDatabase().inTransaction(workspace.getCurrentBranch(), Transaction.LockType.READ, tx -> {
+            FileSystem fs = tx.getFileSystem();
+            Path dirPath = fs.getPath(dir);
+            Path newWd = dirPath.isAbsolute() ? dirPath : fs.getPath(wd, dir).normalize();
+            if (!Files.isDirectory(newWd)) {
+                throw new IllegalArgumentException("Not a directory " + dir);
+            }
+            return newWd;
+        });
+        wd = wdPath.toString();
+        return wd;
     }
 
 }
