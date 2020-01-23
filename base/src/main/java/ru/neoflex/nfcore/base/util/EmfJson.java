7228b04f90982776609d84a39d9ce2f207e31d22
hedaira <mezzo666@mail.ru>
2020-01-23 11:04:24 +0300

    added in requestHandler from client to server (changed arg.type from List to EList && arg.type.type from Object to EObject)

--- base/src/main/java/ru/neoflex/nfcore/base/util/EmfJson.java
+++ base/src/main/java/ru/neoflex/nfcore/base/util/EmfJson.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.cfg.ContextAttributes;
-import org.eclipse.emf.common.util.ECollections;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.common.util.*;
 import org.eclipse.emf.ecore.*;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -66,11 +64,49 @@ public class EmfJson {
         if (arg == null) {
             return null;
         }
+        if (eType.getName().equals("EStringToStringMapEntry")) {
+            ObjectMapper oMapper = new ObjectMapper();
+            Map<String, String> params = oMapper.convertValue(arg, Map.class);
+            EMap<String, String> updatedParams = new BasicEMap<String, String>();
+            for(Map.Entry<String, String> entry : params.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                updatedParams.put(key, value);
+            }
+            return updatedParams;
+        }
         if (eType instanceof EClass) {
-            Map node = (Map) arg;
-            String ref = (String) node.get("$ref");
-            Resource resource = store.loadResource(ref);
-            return resource.getContents().get(0);
+            EClass eClass = (EClass) eType;
+            if (arg instanceof Map) {
+                Map node = (Map) arg;
+                String ref = (String) node.get("$ref");
+                if (ref != null) {
+                    Resource resource = store.loadResource(ref);
+                    return resource.getContents().get(0);
+                }
+                else {
+                    EObject eObject = EcoreUtil.create(eClass);
+                    for (EAttribute attr: eClass.getEAllAttributes()) {
+                        if (node.containsKey(attr.getName())) {
+                            eObject.eSet(attr, node.get(attr.getName()));
+                        }
+                    }
+                }
+            }
+            if (arg instanceof List) {
+                List<Map> argList = (List<Map>) arg;
+                EList<EObject> conditions = new BasicEList();
+                for (Map node: argList) {
+                    EObject eObject = EcoreUtil.create(eClass);
+                    for (EAttribute attr: eClass.getEAllAttributes()) {
+                        if (node.containsKey(attr.getName())) {
+                            eObject.eSet(attr, node.get(attr.getName()));
+                        }
+                    }
+                    conditions.add(eObject);
+                }
+                return conditions;
+            }
         }
         if (eType instanceof EDataType) {
             EDataType eDataType = (EDataType) eType;
