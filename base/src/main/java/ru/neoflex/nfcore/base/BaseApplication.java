3e5e8e627df58968052839cbee71a25ebe8fe81e
hedaira <mezzo666@mail.ru>
2020-02-21 15:43:46 +0300

    added Swagger

--- base/src/main/java/ru/neoflex/nfcore/base/BaseApplication.java
+++ base/src/main/java/ru/neoflex/nfcore/base/BaseApplication.java
@@ -11,6 +11,11 @@ import org.springframework.context.annotation.ComponentScan;
 import ru.neoflex.meta.emfgit.TransactionClassLoader;
 import ru.neoflex.nfcore.base.filters.ContextFilter;
 import ru.neoflex.nfcore.base.filters.GitClasspathFilter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -60,5 +65,14 @@ public class BaseApplication {
         params.put("export-all", "1");
         registration.setInitParameters(params);
         return registration;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
