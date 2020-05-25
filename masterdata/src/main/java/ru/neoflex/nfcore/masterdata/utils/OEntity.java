948a11cd73f552ac477ffe961da82b9d7f85483b
arch7tect <arch7tect@gmail.com>
2020-05-25 18:12:04 +0300

    MasterdataView

--- masterdata/src/main/java/ru/neoflex/nfcore/masterdata/utils/OEntity.java
+++ masterdata/src/main/java/ru/neoflex/nfcore/masterdata/utils/OEntity.java
@@ -1,4 +1,4 @@
-package ru.neoflex.nfcore.masterdata;
+package ru.neoflex.nfcore.masterdata.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
