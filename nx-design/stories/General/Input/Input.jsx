d8c7320504ec2bc628c153bf2cb3a5c49f7a04f1
itrushchelev <freedrex@gmail.com>
2020-06-03 10:49:04 +0300

    added inputs in storybook

--- nx-design/stories/General/Input/Input.jsx
+++ nx-design/stories/General/Input/Input.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Checkbox, Input, Radio, DatePicker, Select} from "antd";
-import styled from 'styled-components'
-
+import styled from 'styled-components';
+import calendar from '../../../icons/calendar.svg';
 
 const NXInput = styled(Input)`
     background: #FFFFFF;
@@ -16,9 +16,39 @@ const NXInput = styled(Input)`
 `
 
 const NXRadio = styled(Radio)`
+.ant-radio-inner::after {
+  background-color: #2A356C !important;
+  width: 12px;
+  height: 12px;
+}
+.ant-radio-inner {
+  border: 1px solid #2A356C !important;
+  align-items:center;
+  width: 20px;
+  height: 20px;
+}
 `
 
 const NXCheckbox = styled(Checkbox)`
+
+.ant-checkbox-checked .ant-checkbox-inner {
+  background-color: white !important;
+  border: 2px solid #424D78;
+}
+
+.ant-checkbox-wrapper .ant-checkbox-inner,
+.ant-checkbox .ant-checkbox-inner,
+.ant-checkbox-input:focus + .ant-checkbox-inner {
+  border: 2px solid #424D78 !important;
+}
+
+.ant-checkbox-checked .ant-checkbox-inner::after {
+  border-color: #424D78 !important;
+}
+
+.ant-checkbox-checked::after {
+  border: 2px solid #424D78 !important;
+}
 `
 
 const NXTextArea = styled(Input.TextArea)`
@@ -31,7 +61,9 @@ const NXTextArea = styled(Input.TextArea)`
 
 const NXDatePicker = styled(DatePicker)`
     width: ${(props) => (props.width ? `${props.width}` : "auto")};
-
+    .ant-calendar-picker-icon{
+    content: url(${calendar});
+}
 `
 
 const NXSelect = styled(Select)`
--- nx-design/stories/General/index.js
+++ nx-design/stories/General/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import '../../../uibase/src/styles/App.css'
 
 import ButtonPage from './Button';
 import IconPage from './Icon';
