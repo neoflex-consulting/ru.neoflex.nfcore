c193a08cfc74e1ffe29e745f5fa40f931ff082eb
arch7tect <arch7tect@gmail.com>
2020-06-26 18:23:19 +0300

    add resource permissions

--- base/src/main/groovy/ru/neoflex/nfcore/base/auth/impl/AuthFactoryExt.groovy
+++ base/src/main/groovy/ru/neoflex/nfcore/base/auth/impl/AuthFactoryExt.groovy
@@ -3,18 +3,4 @@ package ru.neoflex.nfcore.base.auth.impl
 import ru.neoflex.nfcore.base.auth.*
 
 class AuthFactoryExt extends AuthFactoryImpl{
-    @Override
-    AllPermission createAllPermission() {
-        return new AllPermissionExt()
-    }
-
-    @Override
-    ObjectPermission createObjectPermission() {
-        return new ObjectPermissionExt()
-    }
-
-    @Override
-    Role createRole() {
-        return new RoleExt()
-    }
 }
