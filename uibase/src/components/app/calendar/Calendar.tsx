e95dd2146c0f9147318937da968f0c40883d2f54
hedaira <mezzo666@mail.ru>
2020-06-29 14:38:27 +0300

    fix bug with Calendar

--- uibase/src/components/app/calendar/Calendar.tsx
+++ uibase/src/components/app/calendar/Calendar.tsx
@@ -133,24 +133,26 @@ class Calendar extends React.Component<any, any> {
     };
 
     getAllNotificationInstances(currentMonth: Date, updateViewObject: boolean) {
-        const monthStart = dateFns.startOfMonth(currentMonth);
-        const monthEnd = dateFns.endOfMonth(monthStart);
-        const dateFrom = monthStart.toString();
-        const dateTo = monthEnd.toString();
-        const ref: string = this.props.viewObject.eURI();
-        const methodName: string = 'getNotificationInstances';
+        if (this.props.viewObject.get('yearBook') !== null) {
+            const monthStart = dateFns.startOfMonth(currentMonth);
+            const monthEnd = dateFns.endOfMonth(monthStart);
+            const dateFrom = monthStart.toString();
+            const dateTo = monthEnd.toString();
+            const ref: string = this.props.viewObject.eURI();
+            const methodName: string = 'getNotificationInstances';
 
-        if (updateViewObject && this.state.classAppModule !== undefined) {
-            API.instance().findByKindAndName(this.state.classAppModule, this.props.context.viewObject.eContainer.get('name'), 999)
-                .then((result) => {
-                    this.props.context.updateContext(({viewObject: result[0].eContents()[0].get('view')}))
-                })
+            if (updateViewObject && this.state.classAppModule !== undefined) {
+                API.instance().findByKindAndName(this.state.classAppModule, this.props.context.viewObject.eContainer.get('name'), 999)
+                    .then((result) => {
+                        this.props.context.updateContext(({viewObject: result[0].eContents()[0].get('view')}))
+                    })
+            }
+
+            API.instance().call(ref, methodName, [dateFrom, dateTo]).then((result: any) => {
+                let notificationInstancesDTO = JSON.parse(result).resources;
+                this.setState({notificationInstancesDTO, spinnerVisible: false});
+            });
         }
-
-        API.instance().call(ref, methodName, [dateFrom, dateTo]).then((result: any) => {
-            let notificationInstancesDTO = JSON.parse(result).resources;
-            this.setState({notificationInstancesDTO, spinnerVisible: false});
-        });
     };
 
     getAllStatuses() {
@@ -445,7 +447,7 @@ class Calendar extends React.Component<any, any> {
 
     private getTitle(day: any) {
         let temp: any = [];
-            this.props.viewObject.get('yearBook').get('days').array().filter((r: any) =>
+        this.props.viewObject.get('yearBook') !== null && this.props.viewObject.get('yearBook').get('days').array().filter((r: any) =>
                 dateFns.isSameYear(day, dateFns.parseISO(r.get('date')))
                 && dateFns.isSameMonth(day, dateFns.parseISO(r.get('date')))
                 && dateFns.isSameDay(day, dateFns.parseISO(r.get('date')))
@@ -1043,10 +1045,24 @@ class Calendar extends React.Component<any, any> {
         }
         return (
             <div>
+                {this.calendarNotifications()}
                 <div className="body">{rows}</div>
             </div>
         )
     }
+
+    calendarNotifications = () => {
+        let description = []
+        if (this.props.viewObject.get('yearBook') === null) {
+            description.push('YearBook not connected to calendar')
+        }
+        if (this.props.viewObject.get('notifications').array().length === 0) {
+            description.push(' Calendar does not contain notifications')
+        }
+        if (description.length !== 0) {
+            this.props.context.notification('Calendar', description.toString(), 'info')
+        }
+    };
 
     render() {
         return (
