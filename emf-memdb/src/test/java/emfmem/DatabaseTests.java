d9ac0a039379efb682c7da999c40b981a71cffcc
arch7tect <arch7tect@gmail.com>
2020-07-13 18:46:09 +0300

    tests

---
+++ emf-memdb/src/test/java/emfmem/DatabaseTests.java
@@ -0,0 +1,68 @@
+package emfmem;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ru.neoflex.meta.emfmemdb.MemBDServer;
+import ru.neoflex.meta.test.Group;
+import ru.neoflex.meta.test.TestFactory;
+import ru.neoflex.meta.test.User;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DatabaseTests extends TestBase {
+    @Before
+    public void startUp() throws Exception {
+        memBDServer = refreshRatabase();
+    }
+
+    @Test
+    public void createEMFObject() throws Exception {
+        Group group = TestFactory.eINSTANCE.createGroup();
+        String[] ids = memBDServer.inTransaction(false, (MemBDServer.TxFunction<String[]>) tx -> {
+            group.setName("masters");
+            ResourceSet resourceSet = tx.createResourceSet();
+            Resource groupResource = resourceSet.createResource(memBDServer.createResourceURI(Stream.empty()));
+            groupResource.getContents().add(group);
+            groupResource.save(null);
+            String groupId = MemBDServer.getIds(groupResource.getURI()).collect(Collectors.joining(","));
+            User user = TestFactory.eINSTANCE.createUser();
+            user.setName("Orlov");
+            user.setGroup(group);
+            Resource userResource = resourceSet.createResource(memBDServer.createResourceURI(Stream.empty()));
+            userResource.getContents().add(user);
+            userResource.save(null);
+            String userId = MemBDServer.getIds(userResource.getURI()).collect(Collectors.joining(","));;
+            Assert.assertNotNull(userId);
+            return new String[] {userId, groupId};
+        });
+        memBDServer.inTransaction(false, (MemBDServer.TxFunction<Void>) tx -> {
+            ResourceSet resourceSet = tx.createResourceSet();
+            Resource userResource = resourceSet.createResource(memBDServer.createResourceURI(Stream.of(tx.get(ids[0]))));
+            userResource.load(null);
+            User user = (User) userResource.getContents().get(0);
+            user.setName("Simanihin");
+            userResource.save(null);
+            return null;
+        });
+        memBDServer.inTransaction(true, (MemBDServer.TxFunction<Void>) tx -> {
+            User user = TestFactory.eINSTANCE.createUser();
+            user.setName("Orlov");
+            user.setGroup(group);
+            ResourceSet resourceSet = tx.createResourceSet();
+            Resource userResource = resourceSet.createResource(memBDServer.createResourceURI(Stream.empty()));
+            userResource.getContents().add(user);
+            userResource.save(null);
+            return null;
+        });
+//        memBDServer.inTransaction(true, (MemBDServer.TxFunction<Void>) tx -> {
+//            return null;
+//        });
+//        memBDServer.inTransaction(true, (MemBDServer.TxFunction<Void>) tx -> {
+//            return null;
+//        });
+    }
+}
---
+++ emf-memdb/src/test/java/emfmem/TestBase.java
@@ -0,0 +1,36 @@
+package emfmem;
+
+import org.eclipse.emf.ecore.EPackage;
+import ru.neoflex.meta.emfmemdb.MemBDServer;
+import ru.neoflex.meta.test.TestPackage;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class TestBase {
+    public static final String MEMDB = "test-emf-mem";
+    MemBDServer memBDServer;
+
+    public static boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
+    }
+
+    public static MemBDServer getDatabase() throws Exception {
+        return new MemBDServer(getDatabaseFile().getAbsolutePath(), MEMDB, new ArrayList<EPackage>(){{add(TestPackage.eINSTANCE);}});
+    }
+
+    public static File getDatabaseFile() {
+        return new File(System.getProperty("user.home") + "/.memdb", MEMDB);
+    }
+
+    public static MemBDServer refreshRatabase() throws Exception {
+        deleteDirectory(getDatabaseFile());
+        return getDatabase();
+    }
+}
