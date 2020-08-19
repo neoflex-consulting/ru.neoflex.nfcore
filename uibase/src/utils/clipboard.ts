665aeae742944b6dc25c883445b01faf46c7e6bc
unk774 <nwmfmm@gmail.com>
2020-08-19 22:07:07 +0400

    developer copy/paste (permission workaround)

--- uibase/src/utils/clipboard.ts
+++ uibase/src/utils/clipboard.ts
@@ -1,23 +1,52 @@
+async function checkPermissions() {
+    let isReadAllowed, isWriteAllowed = false;
+    //https://github.com/microsoft/TypeScript/issues/33923
+    // @ts-ignore
+    await navigator.permissions.query({name: 'clipboard-read'}).then((permissionStatus:any) => {
+        console.log(permissionStatus.state);
+        if (permissionStatus.state !== "denied")
+            isReadAllowed = true
+    });
+    // @ts-ignore
+    await navigator.permissions.query({name: 'clipboard-write'}).then((permissionStatus:any) => {
+        console.log(permissionStatus.state);
+        if (permissionStatus.state !== "denied")
+            isWriteAllowed = true
+    });
+    return isReadAllowed && isWriteAllowed;
+}
+
 async function copyToClipboard(stringToCopy: string) {
-    await navigator.clipboard.writeText(stringToCopy);
-    console.log('Content copied to clipboard');
+    checkPermissions().then(async isAllowed=>{
+        if (isAllowed) {
+            await navigator.clipboard.writeText(stringToCopy);
+            console.log('Content copied to clipboard');
+        } else {
+            document.addEventListener('copy', function(event){
+                event.clipboardData!.setData('text/plain', stringToCopy);
+                event.preventDefault(); // default behaviour is to copy any selected text
+                document.removeEventListener('copy',()=>{});
+            });
+            document.execCommand('copy');
+        }
+    })
 }
 
 async function getClipboardContents() {
     let json = "";
-    if (navigator.clipboard) {
-        json = await navigator.clipboard.readText();
-        console.log('Pasted content: ', json);
-    } else {
-        navigator.permissions.query({
-            name: "clipboard"
-        }).then((permissionStatus:any) => {
-            console.log(permissionStatus.state);
-            permissionStatus.onchange = () => {
-                console.log(permissionStatus.state);
-            };
-        });
-    }
+    await checkPermissions().then(async isAllowed=>{
+        if (isAllowed) {
+            json = await navigator.clipboard.readText();
+            console.log('Pasted content: ', json);
+        } else {
+            document.addEventListener('copy', function(event){
+                json = event.clipboardData!.getData('text/plain');
+                event.preventDefault(); // default behaviour is to copy any selected text
+                document.removeEventListener('copy',()=>{});
+            });
+            document.execCommand('copy');
+        }
+    });
     return json
 }
 
