e09ef9f050f188f1f721017cecfc51b3b151a894
Ampelia <dbaranov@neoflex.ru>
2020-10-07 14:00:19 +0300

    fixing Bars

--- uibase/src/components/app/calendar/Calendar.tsx
+++ uibase/src/components/app/calendar/Calendar.tsx
@@ -858,7 +858,7 @@ class Calendar extends React.Component<any, any> {
 
                         <NeoButton
                             title={t('legend')}
-                            style={{width: '24px', height: '24px', color: '#6e6e6e'}}
+                            style={{width: '24px', height: '24px', color: '#6e6e6e',marginTop: "4px"}}
                             type="link"
                             onClick={this.handleLegendMenu}>
                             <NeoIcon icon={'legend'} size={'m'} />
@@ -910,16 +910,17 @@ class Calendar extends React.Component<any, any> {
                     </div>
                 }
 
-                <div className="verticalLine" style={{borderLeft: '1px solid #B3B3B3', marginLeft: '18px', marginRight: '18px', height: '40px'}}/>
+                <div className="verticalLine" style={{borderLeft: '1px solid #B3B3B3', marginLeft: '16px', marginRight: '16px', height: '40px'}}/>
 
                     <NeoButton
+                        style={{ marginTop: "4px",}}
                         title={t('add event')}
                         type="link"
                         onClick={this.handleCreateMenu}>
                         <NeoIcon icon={'plus'} color={'#5E6785'} size={'m'} />
                     </NeoButton>
 
-                <div className="verticalLine" style={{borderLeft: '1px solid #B3B3B3', marginLeft: '18px', marginRight: '16px', height: '40px'}}/>
+                <div className="verticalLine" style={{borderLeft: '1px solid #B3B3B3', marginLeft: '16px', marginRight: '16px', height: '40px'}}/>
                 <NeoButton
                     title={t('calendar')}
                     type={this.state.calendarVisible ? 'disabled' : "link"}
