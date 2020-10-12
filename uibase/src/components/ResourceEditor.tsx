9ef97e2e0fb96378958d414673fda51c9b42d3b8
hedaira <mezzo666@mail.ru>
2020-10-12 17:14:07 +0300

    added edit in ResourceEditor

--- uibase/src/components/ResourceEditor.tsx
+++ uibase/src/components/ResourceEditor.tsx
@@ -21,6 +21,7 @@ interface ITargetObject {
 }
 
 export interface Props {
+    principal: any
 }
 
 interface State {
@@ -51,7 +52,8 @@ interface State {
     modalApplyChangesVisible: Boolean,
     clipboardObject: ITargetObject,
     edit: boolean,
-    expandedKeys: string[]
+    expandedKeys: string[],
+    saveMenuVisible: boolean
 }
 
 
@@ -80,7 +82,7 @@ const getChildNode = (children: any[], nodeKey:string) => {
     return retVal
 };
 
-class ResourceEditor extends React.Component<any, State> {
+class ResourceEditor extends React.Component<Props & WithTranslation & any, State> {
 
     private splitterRef: React.RefObject<any>;
     private treeRef: React.RefObject<any>;
@@ -118,7 +120,8 @@ class ResourceEditor extends React.Component<any, State> {
         isClipboardValidObject: false,
         clipboardObject: { eClass: "" },
         edit: false,
-        expandedKeys: []
+        expandedKeys: [],
+        saveMenuVisible: false
     };
 
     refresh = (refresh: boolean): void => {
@@ -155,7 +158,8 @@ class ResourceEditor extends React.Component<any, State> {
             mainEObject: mainEObject,
             resourceJSON: nestedJSON,
             targetObject: findObjectById(nestedJSON, '/'),
-            resource: resource
+            resource: resource,
+            edit: true
         })
     }
 
@@ -175,7 +179,7 @@ class ResourceEditor extends React.Component<any, State> {
                     //show information from an old targetObject. To prevent those side effects we have to null targetObject and tableData.
                     targetObject: { eClass: "" },
                     tableData: []
-                }))
+                }));
             })
             :
             this.generateEObject()
@@ -762,27 +766,31 @@ class ResourceEditor extends React.Component<any, State> {
         }
     };
 
-    changeEdit = () => {
+    changeEdit = (redirect: boolean) => {
         if (this.state.edit) {
             API.instance().deleteLock(this.state.mainEObject._id)
                 .then(() => {
+                    this.save(false, redirect);
                     this.setState({edit: false});
-                    this.refresh(true)
                 })
                 .catch(() => {
+                    this.save(false, redirect);
                     this.setState({edit: false});
-                    this.refresh(true)
                 })
         }
         else {
             this.state.mainEObject._id !== undefined && API.instance().createLock(this.state.mainEObject._id)
-                .then(() => {});
-            this.setState({edit: true});
-            this.refresh(true)
+                .then(() => {
+                    this.setState({edit: true});
+                    this.refresh(true)
+                })
         }
     };
 
-    save = (redirectAfterSave:boolean = false) => {
+    save = (redirectAfterSave:boolean = false, saveAndExit:boolean = false) => {
+        if (this.state.targetObject === undefined) {
+            this.setState({edit: false})
+        }
         this.state.mainEObject.eResource().clear();
         const resource = this.state.mainEObject.eResource().parse(this.state.resourceJSON as Ecore.EObject);
         if (resource) {
@@ -797,9 +805,13 @@ class ResourceEditor extends React.Component<any, State> {
                     targetObject: updatedTargetObject ? updatedTargetObject : this.state.targetObject,
                     resource: resource
                 });
-                this.props.history.push(`/developer/data/editor/${resource.get('uri')}/${resource.rev}`);
-                if (redirectAfterSave)
-                    this.redirect()
+                if (!saveAndExit) {
+                    this.props.history.push(`/developer/data/editor/${resource.get('uri')}/${resource.rev}`);
+                    this.refresh(true)
+                }
+                if (redirectAfterSave) {
+                    this.redirect();
+                }
             }).catch(() => {
                 this.setState({ isSaving: false, isModified: true })
             })
@@ -832,6 +844,7 @@ class ResourceEditor extends React.Component<any, State> {
     };
 
     componentWillUnmount() {
+        this.changeEdit(true);
         window.removeEventListener("click", this.hideRightClickMenu);
         window.removeEventListener("keydown", this.saveOnCtrlS)
     }
@@ -865,9 +878,16 @@ class ResourceEditor extends React.Component<any, State> {
                     API.instance().findByKind(result,  {contents: {eClass: result.eURI()}})
                         .then((result: Ecore.Resource[]) => {
                             if (result.length !== 0) {
-                                let currentLockFile = result.filter( (r: EObject) => r.eContents()[0].get('name') === this.state.mainEObject._id);
+                                let currentLockFile = result
+                                    .filter( (r: EObject) =>
+                                        (r.eContents()[0].get('name') === this.state.mainEObject._id &&
+                                            r.eContents()[0].get('audit').get('createdBy') === this.props.principal.name)
+                                    );
                                 if (currentLockFile.length !== 0) {
                                     this.setState({edit: true});
+                                }
+                                else {
+                                    this.setState({edit: false});
                                 }
                             }
                         })
@@ -877,7 +897,7 @@ class ResourceEditor extends React.Component<any, State> {
 
     private saveOnCtrlS = (event: any) => {
         if (event.ctrlKey && event.code === 'KeyS') {
-            this.save();
+            this.save(false, false);
             event.preventDefault();
         }
     };
@@ -892,11 +912,16 @@ class ResourceEditor extends React.Component<any, State> {
                 </Helmet>
                 <FetchSpinner/>
                 <Layout.Header className="head-panel">
-                    <Button className="panel-button" icon="edit" onClick={ ()=> this.changeEdit()} title={this.props.t("edit")} />
                     {
-                        this.state.edit && (this.state.isSaving
+                        this.state.mainEObject._id !== undefined && (this.state.edit ?
+                            <Button className="panel-button" icon="arrow-left" onClick={ ()=> this.changeEdit(false)} title={this.props.t("apply and quit")} />
+                            :
+                            <Button className="panel-button" icon="edit" onClick={ ()=> this.changeEdit(false)} title={this.props.t("edit")} />)
+                    }
+                    {
+                        (this.state.edit || this.state.mainEObject._id === undefined) && (this.state.isSaving
                             ? <Icon className="panel-icon" type="loading"/>
-                            : <Button className="panel-button" icon="save" onClick={()=>this.save(false)} title={this.props.t("save")}/>)
+                            : <Button className="panel-button" icon="save" onClick={()=>this.save(false, false)} title={this.props.t("save")}/>)
                     }
                     <Button className="panel-button" icon="reload" onClick={ ()=> this.refresh(true)} title={this.props.t("refresh")} />
                     {this.state.resource.get && this.state.resource.get('uri') &&
@@ -1096,7 +1121,7 @@ class ResourceEditor extends React.Component<any, State> {
                         <br/>
                         <br/>
                         <div>
-                            <Button onClick={()=>this.save(true)}>
+                            <Button onClick={()=>this.save(true, false)}>
                                 {t("apply and run")}
                             </Button>
                             <Button onClick={this.handleApplyChangesModalCancel}>
