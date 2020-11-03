301e6fc5333b34cb56e7b850c1ee6f51f279d14b
unk774 <nwmfmm@gmail.com>
2020-11-03 19:02:44 +0400

    https://jira-new.neoflex.ru/browse/NFDGNRSDEV-93

--- uibase/src/components/ResourceEditor.tsx
+++ uibase/src/components/ResourceEditor.tsx
@@ -20,6 +20,8 @@ import moment from 'moment';
 import FetchSpinner from "./FetchSpinner";
 import {Helmet} from "react-helmet";
 import {copyToClipboard, getClipboardContents} from "../utils/clipboard";
+import {getClassAnnotationByClassAndKey} from "../utils/eCoreUtil";
+import './../styles/ResouceEditor.css'
 
 
 interface ITargetObject {
@@ -217,12 +219,14 @@ class ResourceEditor extends React.Component<Props & WithTranslation & any, Stat
             return eClass.get('eAllStructuralFeatures') && eClass.get('eAllStructuralFeatures').map((feature: Ecore.EObject, idx: Number) => {
                     const isContainment = Boolean(feature.get('containment'));
                     const upperBound = feature.get('upperBound');
+                    const invisibleFields = `,${getClassAnnotationByClassAndKey(feature.eContainer && feature.eContainer, 'invisible', true)},`;
                     if ((upperBound === -1 || upperBound === 1) && isContainment) {
                         const targetObject: { [key: string]: any } = Array.isArray(json[feature.get('name')]) ?
                             json[feature.get('name')]
                             :
                             json[feature.get('name')] ? [json[feature.get('name')]] : [];
                         return <Tree.TreeNode
+                            className={invisibleFields.includes(`,${feature.get('name')},`) ? "hidden-leaf" : ""}
                             parentUpdater={json.updater}
                             upperBound={upperBound}
                             isArray={true}
@@ -462,17 +466,24 @@ class ResourceEditor extends React.Component<Props & WithTranslation & any, Stat
     prepareTableData(targetObject: { [key: string]: any; }, mainEObject: Ecore.EObject, key: String): Array<any> {
         const preparedData: Array<Object> = [];
         let featureList: any = undefined;
+        let invisibleFields = "";
+        let disableFields = "";
+        let test = "";
         if (mainEObject.eContainer.getEObject(targetObject._id) !== null && mainEObject.eContainer.getEObject(targetObject._id) !== undefined) {
-            featureList = mainEObject.eContainer.getEObject(targetObject._id).eClass.get('eAllStructuralFeatures')
-        }
+            featureList = mainEObject.eContainer.getEObject(targetObject._id).eClass.get('eAllStructuralFeatures');
+            invisibleFields = `,${getClassAnnotationByClassAndKey(mainEObject.eContainer.getEObject(targetObject._id).eClass, 'invisible', true)},`;
+            disableFields = `,${getClassAnnotationByClassAndKey(mainEObject.eContainer.getEObject(targetObject._id).eClass, 'disabled', true)},`;
+                    }
         else if (targetObject._id === undefined && mainEObject.eContainer.eContents().length !== 0) {
-            featureList = mainEObject.eContainer.eContents()[0].eClass.get('eAllStructuralFeatures')
+            featureList = mainEObject.eContainer.eContents()[0].eClass.get('eAllStructuralFeatures');
+            invisibleFields = `,${getClassAnnotationByClassAndKey(mainEObject.eContainer.eContents()[0].eClass, 'invisible', true)},`;
+            disableFields = `,${getClassAnnotationByClassAndKey(mainEObject.eContainer.eContents()[0].eClass, 'disabled', true)},`;
         }
         if (featureList !== undefined) {
             featureList.forEach((feature: Ecore.EObject, idx: Number) => {
                 const isContainment = Boolean(feature.get('containment'));
                 const isContainer = feature.get('eOpposite') && feature.get('eOpposite').get('containment') ? true : false;
-                if (!isContainment && !isContainer) preparedData.push({
+                if (!isContainment && !isContainer && !invisibleFields.includes(`,${feature.get('name')},`)) preparedData.push({
                     property: feature.get('name'),
                     value: FormComponentMapper.getComponent({
                         value: targetObject[feature.get('name')],
@@ -488,7 +499,7 @@ class ResourceEditor extends React.Component<Props & WithTranslation & any, Stat
                         onEClassBrowse: this.onEClassBrowse,
                         onBrowse: this.onBrowse,
                         mainEObject: mainEObject,
-                        edit: this.state.edit
+                        edit: this.state.edit && !disableFields.includes(`,${feature.get('name')},`)
                     }),
                     key: feature.get('name') + idx
                 })
