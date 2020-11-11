5d4e6c9b574192e2bee81f3ff1318bbd2b2cbf32
unk774 <nwmfmm@gmail.com>
2020-11-11 11:52:40 +0400

    https://jira-new.neoflex.ru/browse/NFDGNRSDEV-66 url decoder

--- uibase/src/components/app/dataset/DatasetGrid.tsx
+++ uibase/src/components/app/dataset/DatasetGrid.tsx
@@ -62,6 +62,7 @@ interface Props {
     valueFormatter?: (params: ValueFormatterParams)=>string|undefined;
     excelCellMask?: (params: ValueFormatterParams)=>string|undefined;
     className?: any;
+    hidePagination?: boolean
 }
 
 class DatasetGrid extends React.Component<Props & any, any> {
@@ -930,7 +931,7 @@ class DatasetGrid extends React.Component<Props & any, any> {
                         />
                     </ConfigProvider>
                     }
-                    <div id="datasetPaginator"
+                    {!this.props.hidePagination && <div id="datasetPaginator"
                          style={{float: "right", opacity: this.state.isGridReady ? 1 : 0, width: "100%", minWidth: "375px", backgroundColor: "#E6E6E6"}}>
                         <Paginator
                             {...this.props}
@@ -940,7 +941,7 @@ class DatasetGrid extends React.Component<Props & any, any> {
                             totalNumberOfRows = {this.state.rowData.filter((r:{[key: string]: unknown})=>r.isVisible__).length}
                             grid = {this.grid}
                         />
-                    </div>
+                    </div>}
                 </div>
             </div>
         )
