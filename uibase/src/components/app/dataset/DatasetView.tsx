2ab1a1ef7a6e375187967b040dbd102ac7fdb6d9
itrushchelev <freedrex@gmail.com>
2021-02-02 16:30:21 +0300

    DatasetComponent tooltip fix

--- uibase/src/components/app/dataset/DatasetView.tsx
+++ uibase/src/components/app/dataset/DatasetView.tsx
@@ -369,7 +369,7 @@ class DatasetView extends React.Component<any, State> {
                     }
                 });
                 rowData.set('valueFormatter', this.valueFormatter);
-                rowData.set('tooltipField', c.get('showTooltipField') ? c.get('datasetColumnTooltip').get('name') : undefined);
+                rowData.set('tooltipField', (c.get('showTooltipField')&&c.get('datasetColumnTooltip')) ? c.get('datasetColumnTooltip').get('name') : undefined);
                 rowData.set('convertDataType', c.get('datasetColumn') ? c.get('datasetColumn').get('convertDataType') : undefined);
                 //передаётся в DatasetGrid для подключения typography к заголоку грида
                 /*rowData.set('customHeader',c.get('headerName'));*/
