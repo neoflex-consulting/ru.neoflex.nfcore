1fa0416c57bea150336a23077cf5334f52170888
arch7tect <arch7tect@gmail.com>
2021-02-03 18:40:20 +0300

    backup/restore

--- base/src/main/java/ru/neoflex/nfcore/base/commands/OrientDBCommands.java
+++ base/src/main/java/ru/neoflex/nfcore/base/commands/OrientDBCommands.java
@@ -39,8 +39,19 @@ public class OrientDBCommands {
     }
 
     @ShellMethod("Import database")
-    public String odbImport(File file, @ShellOption(defaultValue="false") boolean merge) throws IOException {
-        provider.getServer().importDatabase(file, merge);
+    public String odbImport(File file,
+                            @ShellOption(defaultValue = "true") Boolean deleteRIDMapping,
+                            @ShellOption(defaultValue = "true") Boolean preserveClusterIDs,
+                            @ShellOption(defaultValue = "false") Boolean merge,
+                            @ShellOption(defaultValue = "true") Boolean migrateLinks,
+                            @ShellOption(defaultValue = "true") Boolean rebuildIndexes) throws IOException {
+        StringBuffer options = new StringBuffer();
+        if (deleteRIDMapping != null) options.append("-deleteRIDMapping").append("=").append(deleteRIDMapping).append(" ");
+        if (deleteRIDMapping != null) options.append("-preserveClusterIDs").append("=").append(preserveClusterIDs).append(" ");
+        if (deleteRIDMapping != null) options.append("-merge").append("=").append(merge).append(" ");
+        if (deleteRIDMapping != null) options.append("-migrateLinks").append("=").append(migrateLinks).append(" ");
+        if (deleteRIDMapping != null) options.append("-rebuildIndexes").append("=").append(rebuildIndexes).append(" ");
+        provider.getServer().importDatabase(file, options.toString());
         return "Done.";
     }
 
@@ -48,6 +59,31 @@ public class OrientDBCommands {
     public List<String> odbListExports() {
         List<String> result = new ArrayList<>();
         File exportDir = new File(provider.getServer().getHome(), "exports");
+        for (File file: exportDir.listFiles()) {
+            if (file.getName().startsWith(provider.getServer().getDbName())) {
+                result.add(file.getAbsolutePath().replace("\\", "/"));
+            }
+        }
+        result.sort(String::compareTo);
+        return result;
+    }
+
+    @ShellMethod("Backup database")
+    public String odbBackup(@ShellOption(defaultValue=ShellOption.NULL) File file) throws IOException {
+        File export = file == null ? provider.getServer().backupDatabase() : provider.getServer().backupDatabase(file);
+        return "Backed up " + export.getAbsolutePath();
+    }
+
+    @ShellMethod("Restore database")
+    public String odbRestore(@ShellOption(defaultValue=ShellOption.NULL) File file) throws IOException {
+        provider.getServer().restoreDatabase(file);
+        return "Restored " + file.getAbsolutePath();
+    }
+
+    @ShellMethod("List database backups")
+    public List<String> odbListBackups() {
+        List<String> result = new ArrayList<>();
+        File exportDir = new File(provider.getServer().getHome(), "backups");
         for (File file: exportDir.listFiles()) {
             if (file.getName().startsWith(provider.getServer().getDbName())) {
                 result.add(file.getAbsolutePath().replace("\\", "/"));
