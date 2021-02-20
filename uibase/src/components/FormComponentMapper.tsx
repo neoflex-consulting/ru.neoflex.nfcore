2f25f4e426ed2e14912e714f80628a50ea48169c
itrushchelev <freedrex@gmail.com>
2021-02-20 16:33:05 +0300

    antd4 refactoring

--- uibase/src/components/FormComponentMapper.tsx
+++ uibase/src/components/FormComponentMapper.tsx
@@ -4,7 +4,7 @@ import {DatePicker, Input, Select} from 'antd';
 import moment from 'moment';
 
 import {boolSelectionOption, convertPrimitiveToString} from './../utils/resourceEditorUtils';
-import {NeoButton, NeoModal, NeoTag} from "neo-design/lib";
+import {NeoButton, NeoModal, NeoOption, NeoSelect, NeoTag} from "neo-design/lib";
 import './../styles/ComponentMapper.css'
 import {NeoIcon} from "neo-icon/lib";
 import AceEditor from "react-ace";
@@ -149,9 +149,9 @@ function SelectRefObject(props: SelectRefObjectProps): JSX.Element {
             value.map((el: { [key: string]: any }, idx: number) =>
                 <NeoTag
                     className={`${!edit ? "disabled" : undefined} select-ref-object`}
-                    // onClose={(e: any) => {
-                    //     props.handleDeleteRef && props.handleDeleteRef!(el, eObject.get('name'))
-                    // }}
+                    onClose={() => {
+                        props.handleDeleteRef && props.handleDeleteRef!(el, eObject.get('name'))
+                    }}
                     closable={edit}
                     key={el["$ref"]}
                 >
@@ -161,9 +161,9 @@ function SelectRefObject(props: SelectRefObjectProps): JSX.Element {
             :
             <NeoTag
                 className={`${!edit ? "disabled" : undefined} select-ref-object`}
-                // onClose={(e: any) => {
-                //     props.handleDeleteSingleRef && props.handleDeleteSingleRef!(value, eObject.get('name'))
-                // }}
+                onClose={() => {
+                    props.handleDeleteSingleRef && props.handleDeleteSingleRef!(value, eObject.get('name'))
+                }}
                 closable={edit}
                 key={value["$ref"]}
             >
@@ -237,7 +237,7 @@ function DatePickerComponent(props: DatePickerComponentProps): JSX.Element {
         <DatePicker
             showTime
             key={ukey + "_date_" + idx}
-            // defaultValue={moment(value)}
+            defaultValue={moment(value)}
             onChange={(newValue: any) => {
                 onChange && onChange!(newValue)
             }}
@@ -263,9 +263,9 @@ function SelectComponent(props: SelectComponentProps): JSX.Element {
     const { eType, value, idx, ukey, onChange, upperBound, id, edit, showIcon } = props;
 
     return (
-        <Select
+        <NeoSelect
             className={"select-component"}
-            // mode={upperBound === -1 ? "multiple" : "default"}
+            mode={upperBound === -1 ? "multiple" : undefined}
             value={value}
             key={ukey + "_" + idx}
             style={{ width: "300px" }}
@@ -282,10 +282,10 @@ function SelectComponent(props: SelectComponentProps): JSX.Element {
                     return 0;
                 })
                 .map((obj: Ecore.EObject) =>
-                <Select.Option key={ukey + "_opt_" + obj.get('name') + "_" + id} value={obj.get('name')}>
+                <NeoOption key={ukey + "_opt_" + obj.get('name') + "_" + id} value={obj.get('name')}>
                     <div style={{display:"flex", alignItems: "center"}}>{showIcon && <NeoIcon style={{marginRight:"8px"}} icon={neoIconMap[obj.get('name')] as SvgName}/>}{obj.get('name')}</div>
-                </Select.Option>)}
-        </Select>
+                </NeoOption>)}
+        </NeoSelect>
     )
 }
 
